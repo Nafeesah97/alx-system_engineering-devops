8ee7287a6264e0df0580142126cbed747a0626db
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-07 00:08:00 +0100

    3cr1

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Importing necessary libraries"""
+import requests
+import json
+import sys
+
+"""
+
+
+
+This is a python script using REST API to get
+information about his/her TO DO list progress
+Author: Nafeesah
+"""
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    json_file = f"{employee_id}.json"
+    url = "https://jsonplaceholder.typicode.com/"
+    userurl = f"https://jsonplaceholder.typicode.com/users?id={employee_id}"
+    user = requests.get(userurl).json()[0]
+    tasks = requests.get(url + "todos", params={"userId": employee_id}).json()
+
+    task_list = []
+    task_dic = {}
+    for task in tasks:
+        task_dic["task"] = task['title']
+        task_dic["completed"] = task['completed']
+        task_dic["username"] = user['username']
+        task_list.append(task_dic)
+    json_obj = {}
+    json_obj[f"{employee_id}"] = task_list
+
+    with open(json_file, 'w') as f:
+        json.dump(json_obj, json_file)
