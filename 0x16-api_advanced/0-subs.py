560f5ef40441f68358c1a343bc6a9212394dfc70
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-17 00:42:47 +0100

    0cr4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,10 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    data = requests.get(url, headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}, allow_redirects=False).json()
-    subscribers = data.get("data", {}).get("subscribers", 0)
+    headers = {'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    data = requests.get(url, headers=headers, allow_redirects=False)
+    res = data.json()
+    subscribers = res.get("data", {}).get("subscribers", 0)
 
     return subscribers
