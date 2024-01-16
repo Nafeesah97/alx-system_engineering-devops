2994a7d5b15626af093c36ba28f74ebe565aef6d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-17 00:50:02 +0100

    0cr5

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,7 +15,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}
     data = requests.get(url, headers=headers, allow_redirects=False)
-    print(data.headers.get('Content-Type'))
+    print(data)
     res = data.json()
     subscribers = res.get("data", {}).get("subscribers", 0)
 
