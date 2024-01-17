3def9ce613e5da7f752fbd6ba1cedebdc587b797
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-16 17:06:16 -0800

    0cr13

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,11 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={"User-Agent": "My-User-Agent"}, allow_redirects=False)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
     if r.status_code >= 300:
         return 0
     subs = r.json().get("data").get("subscribers")
