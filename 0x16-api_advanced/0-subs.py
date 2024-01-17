8b8cf062bfc6b6c720d0b048ba874de3bd4865ca
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-17 02:14:35 +0100

    0cr15

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
-"""
-a function that queries the Reddit API
-returns the number of subscribers
-Author: Nafeesah
-"""
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {"User-Agent": "0x16-api_advanced"}
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.json().get("data") is None:
-        return 0
-    subs = r.json().get("data").get("subscribers")
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
     return subs
