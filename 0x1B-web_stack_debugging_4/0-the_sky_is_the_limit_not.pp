09c9e49e9d0d27645fd82a954aac4af3810b0df7
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-27 17:20:44 -0800

    db1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,12 +2,12 @@
 
 # Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit for Holberton user.
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+# Restart nginx
+exec { 'restart-nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/'
 }
